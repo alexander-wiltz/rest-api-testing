c2034ba315dce408f297a46b216342580af7b4b1
alwi1002 <alwi1002@stud.hs-kl.de>
2022-10-31 17:36:36 +0100

    initial commit

---
+++ src/main/java/RESTfulAPI.java
@@ -0,0 +1,12 @@
+public class RESTfulAPI {
+
+    /***
+     * Testing RESTful APIs with JAVA
+     * ref: https://docs.github.com/en/rest
+     *
+     * @author Alexander Wiltz
+     * @date 31-10-2022
+     *
+     */
+
+}
